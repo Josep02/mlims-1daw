46a2e3a856ff8afbab0b8f05cf34b72a3a1f2e06
Josep <josepferrer99@gmail.com>
2022-03-15 10:04:07 +0100

    Task 807

---
+++ Unit 8. An introduction to Javascript/806/scripts/main.js
@@ -0,0 +1,3 @@
+var matches = document.querySelectorAll('p');
+
+
---
+++ Unit 8. An introduction to Javascript/807/scripts/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function main() {
+
+    let book1 = {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        publishedIn: 2008,
+        alreadyRead: true
+    }
+
+    let book2 = {
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        publishedIn: 2002,
+        alreadyRead: false
+    }
+
+}
