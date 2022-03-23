bc07d2bbc41002ff4ea36c675b71410fabafd1b4
Josep <josepferrer99@gmail.com>
2022-03-23 09:50:14 +0100

    Task 808 and 809

--- Unit 8. An introduction to Javascript/807/scripts/main.js
+++ Unit 8. An introduction to Javascript/807/scripts/main.js
@@ -7,13 +7,35 @@ function main() {
         author: "J.R.R. Tolkien",
         publishedIn: 2008,
         alreadyRead: true
-    }
+    };
 
     let book2 = {
         title: "The Lord of the Rings",
         author: "J.R.R. Tolkien",
         publishedIn: 2002,
         alreadyRead: false
+    };
+
+
+    document.getElementById("book1").innerHTML=
+        book1.title + " by " + book1.author;
+
+    document.getElementById("book2").innerHTML=
+        book2.title + " by " + book2.author;
+
+/*
+    for (var i = 0; book1.lengh; i ++)
+    
+    if (book1.alreadyRead = false) {
+        document.getElementById("book1").innerHTML =
+        "You still need to read" + book2.title + " by " + book2.author;
     }
 
+
+
+    if (book1.alreadyRead=false)
+            "You still need to read" + book2.title + " by " + book2.author;
+*/
 }
+
+document.addEventListener('DOMContentLoaded', main);
