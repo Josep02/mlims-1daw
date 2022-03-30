8ccbb0f72eb406323c70ccc879fbb300700d6d10
Josep <josepferrer99@gmail.com>
2022-03-30 10:12:40 +0200

    Task 810

---
+++ Unit 8. An introduction to Javascript/810/scripts/main.js
@@ -0,0 +1,26 @@
+let harryPotter = {
+    title: 'Harry Potter 1',
+    author: 'JK Rowling',
+    publishedIn: 1997
+};
+let harryPotter2 = {
+    title: 'Harry Potter 2',
+    author: 'JK Rowling',
+    publishedIn: 1998
+};
+let harryPotter3 = {
+    title: 'Harry Potter 3',
+    author: 'JK Rowling',
+    publishedIn: 1999
+};
+
+let harryPotter4 = {
+    title: 'Harry Potter 4',
+    author: 'JK Rowling',
+    publishedIn: 2001
+};
+
+
+
+let books = [harryPotter, harryPotter2, harryPotter3];
+books.push(harryPotter4);
